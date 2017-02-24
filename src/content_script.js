78b2486117325509450288e10d4cbcbce0279bae
lorderikir <ericjiang97@gmail.com>
2017-02-24 20:29:43 +1100

    initial commit

---
+++ src/content_script.js
@@ -0,0 +1,38 @@
+var headerBar;
+
+function replaceHeader() {
+    // check if element exists yet
+    headerBar = document.querySelector('body > div > .header');
+    if (headerBar) {
+        // element exists, remove the event listeners so we don't run this twice
+        document.removeEventListener('DOMNodeInserted', replaceHeader);
+        document.removeEventListener('DOMContentLoaded', replaceHeader);
+
+
+        headerBar.classList.remove('header-dark');
+
+        // default to always removing the border
+        headerBar.classList.add('great-header');
+
+        // check storage if we want it back
+        chrome.storage.sync.get(['enabled'], function (results) {
+            if (results.enabled || results.enabled === undefined) {
+                 headerBar.classList.add('great-header');
+            }
+        });
+
+        // send a message to the background script to enable the page action
+        chrome.runtime.sendMessage('enable_page_action', function () {});
+    }
+}
+
+// event listeners
+document.addEventListener('DOMNodeInserted', replaceHeader);
+document.addEventListener('DOMContentLoaded', replaceHeader);
+
+// listen for messages from the background script to toggle
+chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+    if (message === 'toggle_style') {
+        headerBar.classList.toggle('great-header');
+    }
+});
