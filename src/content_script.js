dda35b295d3ab2724c129bc1f24b5b31c1c216c3
lorderikir <ericjiang97@gmail.com>
2017-03-04 14:38:26 +1100

    deploy v0.2

--- src/content_script.js
+++ src/content_script.js
@@ -1,18 +1,17 @@
 var headerBar;
 
 function replaceContent() {
-    // check if element exists yet
-    headerBar = document.querySelector('#outer-container > div > div.top-bar');
     
     timetablesHome = document.querySelector("#outer-header > header > div.mon-link-block > div > a");
     timetablesHome.classList.add("ui");
     timetablesHome.classList.add("button");
     timetablesHome.classList.add("btnmainblue");
+
+
+    // check if element exists yet
+    headerBar = document.querySelector('#outer-header > header > div.mon-brand-header > div.wrapper');\
+    headerBar.appendChild(timetablesHome)
     
-    helpmeButton = document.querySelector("body > div.SkipThisFixedPosition.walkme-custom-icon-outer-div.walkme-launcher-id-37098.walkme-custom-launcher-outer-div.walkme-text-launcher.custom-launcher-26825.walkme-icon-root-Launcher-37098.walkme-not-embed > div");  
-    helpmeButton.classList.add("ui");
-    helpmeButton.classList.add("button");
-    helpmeButton.classList.add("btnlightblue");
 }
 
 // event listeners
